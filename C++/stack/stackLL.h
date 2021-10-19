d7d443bc4237038f923dc22e07d3e9ce307e99a7
Ayush Agarwal <agarwal.ayush.0109@gmail.com>
2021-10-19 15:42:38 +0530

    Added header file for Stack implemented with Linkedlist

---
+++ C++/stack/stackLL.h
@@ -0,0 +1,38 @@
+template<typename T>
+class Node {
+    public:
+        T data;
+        Node<T> *next;
+        Node(T d) {
+            data = d;
+        }
+};
+
+template<typename T>
+class Stack {
+    Node<T> *head;
+    public:
+        Stack() {
+            head = NULL;
+        }
+        void push(T data) {
+            Node<T> * n = new Node<T>(data);
+            n->next = head;
+            head = n;
+        }
+
+        void pop() {
+            if(head!=NULL) {
+                Node<T> *temp = head;
+                head = head->next;
+                temp->next = NULL;
+                delete temp;
+            }
+        }
+        bool empty() {
+            return head==NULL;
+        }
+        T top() {
+            return head->data;
+        }
+};
